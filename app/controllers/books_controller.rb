1dff1f3905917dc8894b5669aed7c9eeced34b63
its-ajith <ajithyuva31@gmail.com>
2021-11-08 12:38:54 +0530

    filter search update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,20 @@ class BooksController < ApplicationController
   helper_method :sort_column, :sort_direction
   def index
 
+    params[:category_id] && @books = Book.where(category_id: params[:category_id])
     if (params[:title])
       @books = Book.where("title LIKE ?", "%#{params[:title]}%")
     elsif (params[:category_id])
       @books = Book.where("category_id = ?", params[:category_id])
+    else
+      @books = Book.order(sort_column + " " + sort_direction)
     end
+      
+     
+    
+    
+    
 
-    
-    
     
   end
 
